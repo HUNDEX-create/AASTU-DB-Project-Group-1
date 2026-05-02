498560c51d557f6606b93ccab5ad367566f7c240
Kedir Lemecha <lemechakedir28@gmail.com>
2026-05-02 21:59:39 +0300

    Improve comment formatting in SQL script
    Reformatted comment for clarity and readability.

--- docs/mysql/script.sql.sql
+++ docs/mysql/script.sql.sql
@@ -222,7 +222,8 @@ INSERT INTO Payment (payment_method, payment_date, amount_paid, bill_id) VALUES
 
 
 
---- Financial report: The following SQL query aggregates total payments received for each payment method. This provides a summary of revenue generated through each payment channel and is useful for financial reporting and analysis. ---
+--- Financial report: The following SQL query aggregates total payments received for each payment method.
+This provides a summary of revenue generated through each payment channel and is useful for financial reporting and analysis. ---
 
 SELECT payment_method, SUM(amount_paid) AS total_revenue
 FROM Payment
